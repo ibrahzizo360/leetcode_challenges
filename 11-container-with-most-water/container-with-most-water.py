a43d43c7d910aeb2da4678a9f7bc551d183a3902
Aziz Ibrahim <92813019+ibrahzizo360@users.noreply.github.com>
2024-02-12 20:49:55 +0000

    Time: 530 ms (63.70%) | Memory: 30.2 MB (30.96%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -3,17 +3,14 @@ class Solution:
         waterArea = 0
         i = 0
         j = len(height) - 1
-
         while i < j:
             curArea = (j - i) * min(height[i], height[j])
             if height[i] < height[j]:
                 waterArea = max(curArea, waterArea)
                 i+=1
-
             elif height[i] >= height[j]:
                 waterArea = max(curArea, waterArea)
                 j-=1
-
         return waterArea        
 
             
