69885baebe2d4bf80c29abdbeb7de664259b998a
Aziz Ibrahim <92813019+ibrahzizo360@users.noreply.github.com>
2024-02-14 18:13:50 +0000

    Time: 102 ms (66.89%) | Memory: 18.4 MB (83.69%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -10,19 +10,15 @@ class Solution:
         while l < r:
             max_l = max(max_l, height[l])
             max_r = max(max_r, height[r])
-            print("max_l", max_l)
-            print("max_r", max_r)
 
             if max_l < max_r:
                 calc = min(max_l, max_r) - height[l]
-                result = calc if calc > 0 else 0
-                water += result
+                water += calc if calc > 0 else 0
                 l+=1
 
             else:
                 calc = min(max_l, max_r) - height[r]
-                result = calc if calc > 0 else 0
-                water += result
+                water += calc if calc > 0 else 0
                 r-=1
 
         return water    
