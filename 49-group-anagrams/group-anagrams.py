7d2345460f82c4d78bf42418d6685a76b1a41371
Aziz Ibrahim <92813019+ibrahzizo360@users.noreply.github.com>
2024-08-24 07:56:59 +0000

    Time: 72 ms (58.88%) | Memory: 15.7 MB (67.75%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -8,6 +8,6 @@ class Solution(object):
         for string in strs:
             sorted_string = ''.join(sorted(string))
             hashmap[sorted_string].append(string)
-        return list(hashmap.values())
+        return hashmap.values()
 
         
