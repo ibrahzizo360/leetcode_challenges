ae42f6348fc009ec79ad1386feedf97519843ad0
Aziz Ibrahim <92813019+ibrahzizo360@users.noreply.github.com>
2024-08-24 08:26:22 +0000

    Time: 68 ms (85.89%) | Memory: 15 MB (74.99%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,19 +1,12 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        res = []
-        numsMap = Counter(nums)
-        bucket = [[] for i in range(len(nums) +1)]
-        for num, count in numsMap.items():
-            if len(bucket[count]) == 0:
-                bucket[count] = [num]
-            else:    
-                bucket[count].append(num)    
-        for arr in bucket[::-1]:
-            if len(arr) !=0:
-                for i in arr:
-                    res.append(i)
-            if len(res) == k:
-                return res
-
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        countMap = Counter(nums)
+        res = [item for item, count in countMap.most_common(k)]
+        return res
 
         
