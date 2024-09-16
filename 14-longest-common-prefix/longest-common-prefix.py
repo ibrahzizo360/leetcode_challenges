e3abf92c09d1f084eae247d7724d4ce0c3e32395
Aziz Ibrahim <92813019+ibrahzizo360@users.noreply.github.com>
2024-09-16 00:34:06 +0000

    Time: 22 ms (19.85%) | Memory: 11.9 MB (25.22%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -4,14 +4,10 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
-        
         shortest_str = min(strs, key=len)
-
         for i in range(len(shortest_str)):
-            char = shortest_str[i]
             for string in strs:
-                if char != string[i]:
+                if shortest_str[i] != string[i]:
                     return shortest_str[:i]
         return shortest_str
-
         
