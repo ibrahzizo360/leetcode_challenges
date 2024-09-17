c2bd251e08ac52bd0a127a5096f0d6d8058bd680
Aziz Ibrahim <92813019+ibrahzizo360@users.noreply.github.com>
2024-09-17 22:53:22 +0000

    Time: 16 ms (34.95%) | Memory: 11.7 MB (31.57%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.py
+++ 2491-smallest-even-multiple/smallest-even-multiple.py
@@ -4,9 +4,10 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        for num in range(2,2*150):
-            if num % n == 0 and num % 2 == 0:
-                return num
-        
+        product = n*2
+        if n % n == 0 and n % 2 == 0:
+            return n
+        else:
+            return product
 
         
