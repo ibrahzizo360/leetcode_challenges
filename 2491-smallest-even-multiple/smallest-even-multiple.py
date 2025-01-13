074069213a3f112c377182016e7430b67067f0dc
Aziz Ibrahim <92813019+ibrahzizo360@users.noreply.github.com>
2025-01-13 10:24:33 +0000

    Time: 0 ms (100.00%) | Memory: 17.9 MB (7.41%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.py
+++ 2491-smallest-even-multiple/smallest-even-multiple.py
@@ -1,11 +1,8 @@
 class Solution:
     def smallestEvenMultiple(self, n: int) -> int:
-        multiples = [ n * i for i in range(1, 150)]
-        evenMultiples = []
-        for num in multiples:
-            if num % 2 == 0:
-                evenMultiples.append(num)
-
-        return evenMultiples[0]
+        if n % 2 == 0:
+            return n  # n is already even
+        else:
+            return 2 * n 
 
         
