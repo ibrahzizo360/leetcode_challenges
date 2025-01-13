3552a7795d150e3b20a8f384e6ccc861766af09a
Aziz Ibrahim <92813019+ibrahzizo360@users.noreply.github.com>
2025-01-13 10:18:32 +0000

    Time: 0 ms (100.00%) | Memory: 17.8 MB (26.22%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.py
+++ 2491-smallest-even-multiple/smallest-even-multiple.py
@@ -1,9 +1,11 @@
-class Solution(object):
-    def smallestEvenMultiple(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        return n if n % 2 == 0 else n*2
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        multiples = [ n * i for i in range(1, 150)]
+        evenMultiples = []
+        for num in multiples:
+            if num % 2 == 0:
+                evenMultiples.append(num)
+
+        return evenMultiples[0]
 
         
