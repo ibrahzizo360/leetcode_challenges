a85e22aeb6b7f6a13fa141f91448b920d08b3c6f
Aziz Ibrahim <92813019+ibrahzizo360@users.noreply.github.com>
2025-04-11 09:31:12 +0000

    Time: 4 ms (41.53%) | Memory: 19.1 MB (11.51%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,13 +1,10 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashMap = defaultdict(int)
-        for idx,val in enumerate(nums):
-            diff = target - val
-            if diff in hashMap:
-                return [idx, hashMap[diff]]
-            hashMap[val] = idx   
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashMap:
+                return [hashMap[complement], i]
+            hashMap[nums[i]] = i
+
+        
