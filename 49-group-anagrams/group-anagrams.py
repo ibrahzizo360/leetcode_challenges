305c2b60edb5370c8884bd1d400aee32d2d9fa25
Aziz Ibrahim <92813019+ibrahzizo360@users.noreply.github.com>
2025-04-12 09:55:24 +0000

    Time: 11 ms (83.06%) | Memory: 21.3 MB (39.66%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -5,12 +5,6 @@ class Solution:
         for word in strs:
             key = tuple(sorted(word))
             hashMap[key].append(word)
-            
-            # if sorted(word) in hashMap:
-            #     hashMap[sorted(word)].append(word)
-            # else:
-            #     hashMap[sorted(word)].append(word)
         for v in hashMap.values():
             res.append(v)
-        print(res)
         return res
