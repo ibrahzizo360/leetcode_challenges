e41192a75dcfb838af6e8d952897408fbf0c85b5
Aziz Ibrahim <92813019+ibrahzizo360@users.noreply.github.com>
2025-04-12 09:55:01 +0000

    Time: 15 ms (50.01%) | Memory: 21.6 MB (36.87%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,13 +1,16 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        hashmap = defaultdict(list)
-        for string in strs:
-            sorted_string = ''.join(sorted(string))
-            hashmap[sorted_string].append(string)
-        return hashmap.values()
-
-        
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashMap = defaultdict(list)
+        res = []
+        for word in strs:
+            key = tuple(sorted(word))
+            hashMap[key].append(word)
+            
+            # if sorted(word) in hashMap:
+            #     hashMap[sorted(word)].append(word)
+            # else:
+            #     hashMap[sorted(word)].append(word)
+        for v in hashMap.values():
+            res.append(v)
+        print(res)
+        return res
